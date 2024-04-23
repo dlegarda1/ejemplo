281dba76742f72bb46df6a408020ee18da76b2bd
dlegarda <legarda8027@gmail.com>
2024-04-22 22:29:17 -0500

    practicas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Saludo from './practicas/saludo';
+import Saludo2 from './practicas/Saludo2';
+import Producto from './practicas/Producto';
+import Contador from './practicas/Contador';
+
+const elemento=<h1>Hola Mundo</h1>
+const name='Jorge'
+const elemento2=<h2>Hola {name}</h2>
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {elemento}
+      {elemento2}
+      <Saludo/>
+      <Saludo2/>
+      <Producto nombre="Tablet" descripcion="Es un elemento electrónico" precio={500*3}/>
+      <Producto nombre="Silla" imagen="/logo192.png" alternativo="imagen de silla" descripcion="Es un elemento de oficina" precio={100}/>
+      <Contador/>      
     </div>
   );
 }
